a0a9ad345037e3f059f85fabffa1d8cf50ba2a6c
A02323437 <A02323437@usu.edu>
2025-04-27 13:40:22 -0600

    Making some disperse gesture work for left hand

--- src/GestureLibrary/message_acknowledged.c
+++ src/GestureLibrary/message_acknowledged.c
@@ -3,12 +3,12 @@
 
 GestureState message_acknowledged(IMUState* imu_state)
 {
-    if ((imu_state[THUMB].orientation == FINGER_STRAIGHT) &&
+    if ((imu_state[THUMB].orientation == THUMB_EXTENDED) &&
         (imu_state[MIDDLE].orientation == FINGER_CURLED) &&
         (imu_state[INDEX].orientation == FINGER_CURLED) &&
         (imu_state[RING].orientation == FINGER_CURLED) &&
         (imu_state[PINKY].orientation == FINGER_CURLED) &&
-        (imu_state[BOH].orientation == IMU_RIGHT))
+        (imu_state[BOH].orientation == IMU_LEFT))
     {
         send_gesture_byte('t'); // Send the gesture to the audio module //ascii value of 'F' is 70
         printf("Message Acknowledged gesture detected!\n"); // Print message indicating gesture detection
